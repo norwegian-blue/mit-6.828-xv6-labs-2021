1ee8043b3275255813d949c54e84a2ead2d7c972
Nicola Pompini <nicola.pompini@gmail.com>
2021-12-02 01:09:30 +0100

    uthread lab done

--- user/uthread.c
+++ user/uthread.c
@@ -10,14 +10,34 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
+  char            stack[STACK_SIZE]; /* the thread's stack */
+  int             state;             /* FREE, RUNNING, RUNNABLE */
+  struct context  ctx;               /* Like kernel context structure */
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context*, struct context*);
               
 void 
 thread_init(void)
@@ -62,6 +82,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->ctx, &next_thread->ctx);
   } else
     next_thread = 0;
 }
@@ -75,7 +96,9 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+
+  t->ctx.ra = (uint64) func;          // return new thread to function address
+  t->ctx.sp = (uint64) &(t->stack[STACK_SIZE-1]);   // point to new thread stack
 }
 
 void 
