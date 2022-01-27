e603e1a623dca949dc9ff4425e74c63ce08273f8
Nicola Pompini <nicola.pompini@gmail.com>
2022-01-28 00:17:22 +0100

    create symlink file w/ reference to target path

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -485,10 +485,32 @@ sys_pipe(void)
   return 0;
 }
 
-// 
+// Symbolic link: refer to linked file by pathname.
 uint64
 sys_symlink(void)
 {
-  panic("to be implemented");
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if((argstr(0, target, MAXPATH) < 0) || (argstr(1, path, MAXPATH) < 0))
+    return -1;
+
+  begin_op();
+
+  ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0){
+    end_op();
+    return -1;
+  }
+
+  if(writei(ip, 0, (uint64)&target, 0, strlen(target)) != strlen(target)){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  iunlockput(ip);
+
+  end_op();
+
   return 0;
 }
